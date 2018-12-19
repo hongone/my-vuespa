1c9ef9e1c46d717dcdae3c8a44c279c3596438ce
hongone <6rl@163.com>
2018-12-20 06:26:22 +0800

    调整测试

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,8 +11,8 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
+      .assert.elementPresent('.hello-bem')
+      .assert.containsText('h1', 'Welcome to 海口')
       .assert.elementCount('img', 1)
       .end()
   }
